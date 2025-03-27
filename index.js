c256c71e1f24da3de5855ffb989daf356dc5e288
root <root@vm3270809.firstbyte.club>
2025-03-28 01:22:01 +0300

    fix: add CORS to backend

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 
 app.post('/analyze', async (req, res) => {
